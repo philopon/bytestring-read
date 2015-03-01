b1a1139fa91cae8891cf01a76438f704685cf375
philopon <philopon.dependence@gmail.com>
2015-03-02 06:32:09 +0900

    update benchmark

--- bench/main.hs
+++ bench/main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+import qualified Data.ByteString.Char8 as SC
 import qualified Data.ByteString.Read as R
 import Criterion.Main
 import qualified Data.ByteString.Lex.Double as L
@@ -9,12 +10,13 @@ import qualified Data.Attoparsec.ByteString.Char8 as A
 import Data.Scientific
 
 testB :: ByteString
-testB = "2342395232123424.3424346343524e3"
+testB = "-2342395232123424.3424346343524e3"
 
 main :: IO ()
 main = defaultMain
-    [ bench "read"   $ nf (R.signed R.floating :: ByteString -> Maybe (Double, ByteString)) testB
+    [ bench "bytestring-read"   $ nf (R.signed R.floating :: ByteString -> Maybe (Double, ByteString)) testB
     , bench "text"   $ nf (T.signed T.double . T.decodeUtf8) testB
-    , bench "lexing" $ nf L.readDouble testB
+    , bench "bytestring-lexing" $ nf L.readDouble testB
     , bench "attoparsec" $ nf (\s -> A.parseOnly (fmap toRealFloat A.scientific :: A.Parser Double) s) testB
+    , bench "read" $ nf (\a -> reads $ SC.unpack a :: [(Double, String)]) testB
     ]
