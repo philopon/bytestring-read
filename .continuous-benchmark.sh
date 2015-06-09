f2d4b6c5bbafc7f04ded2d38239a7a29c66bd339
philopon <philopon.dependence@gmail.com>
2015-06-09 23:06:00 +0900

    update .continuous-benchmark.sh

--- .continuous-benchmark.sh
+++ .continuous-benchmark.sh
@@ -9,9 +9,13 @@ git config --global user.email "travis@example.com"
 
 cabal configure --enable-benchmarks
 cabal build bench
+./dist/build/bench/bench -o bench.html
+
 git clone -b gh-pages git@github.com:philopon/bytestring-read.git
-./dist/build/bench/bench -o bytestring-read/continuous.html
 cd bytestring-read
-git add continuous.html
+
+DEST=./continuous/$TRAVIS_BRANCH.html
+mv ../bench.html $DEST
+git add $DEST
 git commit -m "continuous benchmark($TRAVIS_JOB_NUMBER)"
 git push origin gh-pages
